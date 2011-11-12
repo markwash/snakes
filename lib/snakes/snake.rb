c00ed4e9155d4220c7f9bb88ca4263be387b88cf
Mark Washenberger <mark.washenberger@rackspace.com>
2011-11-12 01:56:12 -0500

    need to refactor

--- lib/snakes/snake.rb
+++ lib/snakes/snake.rb
@@ -1,21 +1,142 @@
 class Snake
-  def length
-    return 1
-  end
 
-  def segments
-    return 1
+  attr_reader :direction
+  attr_reader :length
+  attr_reader :points
+  attr_reader :segments
+
+  def initialize(*points)
+    if points.length == 0
+      points = [[0, 0], [0, 0]]
+    elsif points.length == 1
+      points = [points[0], points[0]]
+    end
+    @points = points
+    @segments = SnakeSegments.new(self)
+    @length = calculate_length 
+    @direction = :east
   end
 
   def head
-    return [0, 0]
+    return @points[0]
   end
 
   def tail
-    return [0, 0]
+    return @points[-1]
   end
 
-  def segment(index)
-    return [0, 0]
+  def move
+    if head == tail and @points.length == 2
+      move_point(@points[0], @direction)
+      move_point(@points[-1], @direction)
+      return
+    end
+    neck_direction = Direction.from_points(*@segments[0])
+    @points.unshift(@points[0].clone) if neck_direction != @direction
+    move_point(@points[0], @direction)
+    tail_direction = Direction.from_points(*@segments[-1])
+    move_point(@points[-1], tail_direction)
+    @points.pop() if @points[-1] == @points[-2]
+  end
+    
+  def turn(direction)
+    raise "Invalid direction!" unless Direction.valid?(direction)
+    @direction = direction if direction_currently_possible?(direction)
+  end
+
+  protected
+  def print_points(points)
+    points.each { |x, y| print "(#{x}, #{y}) "}
+    puts
+  end
+
+  def move_point(pt, direction)
+    case direction
+    when :east
+      pt[0] += 1
+    when :west
+      pt[0] -= 1
+    when :north
+      pt[1] += 1
+    when :south
+      pt[1] -= 1
+    end
+  end
+
+  def calculate_length
+    length = 1
+    @segments.each do |pt1, pt2|
+      if pt1[0] == pt2[0]:
+        diff_index = 1
+      elsif pt1[1] == pt2[1]:
+        diff_index = 0
+      else
+        raise "Invalid segment!"
+      end
+      length += (pt1[diff_index] - pt2[diff_index]).abs
+    end
+    return length
+  end
+
+  def direction_currently_possible?(dir)
+    current_angle = Direction.from_points(*@segments[0])
+    return dir != Direction.opposite(current_angle)
+  end
+
+end
+
+class Direction
+
+  VALID_DIRECTIONS = [:east, :west, :north, :south]
+  OPPOSITES = {:east => :west, :west => :east,
+               :north => :south, :south => :north}
+
+  def self.valid?(direction)
+    VALID_DIRECTIONS.include?(direction)
+  end
+
+  def self.from_points(pt1, pt2)
+    return :nil if pt1 == pt2
+    x1, y1 = pt1
+    x2, y2 = pt2
+    return :north if x1 == x2 and y1 > y2
+    return :south if x1 == x2 and y1 < y2
+    return :east if x1 > x2 and y1 == y2
+    return :west if x1 < x2 and y1 == y2
+    raise "Invalid segment! (#{x1}, #{y1}) -> (#{x2}, #{y2})"
+  end
+
+  def self.opposite(direction)
+    return OPPOSITES[direction]
+  end
+
+  private
+  def self.new
+  end
+end
+
+class SnakeSegments
+
+  attr_reader :points
+  
+  def initialize(snake)
+    @snake = snake
+  end
+
+  def length
+    return @snake.points.length - 1
+  end
+
+  def [](index)
+    return [@snake.points[index], @snake.points[index + 1]] if index >= 0
+    return [@snake.points[index - 1], @snake.points[index]] if index < 0
+  end
+
+  def each
+    prev = nil
+    @snake.points.each do |point|
+      yield [prev, point] if not prev.nil?
+      prev = point
+    end
   end
 end
--- test/test_snake.rb
+++ test/test_snake.rb
@@ -1,28 +1,167 @@
 require 'helper'
 
 class TestDefaultSnake < Test::Unit::TestCase
+  def setup
+    @snake = Snake.new
+  end
+
   def test_create_default_snake_length
-    snake = Snake.new
-    assert_equal 1, snake.length
+    assert_equal 1, @snake.length
   end
 
   def test_create_default_snake_head_position
-    snake = Snake.new
-    assert_equal [0, 0], snake.head
+    assert_equal [0, 0], @snake.head
   end
 
   def test_create_default_snake_tail_position
-    snake = Snake.new
-    assert_equal [0, 0], snake.tail
+    assert_equal [0, 0], @snake.tail
   end
 
   def test_create_default_snake_segments
-    snake = Snake.new
-    assert_equal 1, snake.segments
+    assert_equal 1, @snake.segments.length
   end
 
   def test_create_default_snake_segment_0
-    snake = Snake.new
-    assert_equal [0, 0], snake.segment(0)
+    assert_equal [[0, 0], [0, 0]], @snake.segments[0]
+  end
+
+  def test_direction
+    assert_equal :east, @snake.direction
+  end
+
+  def test_turn_cardinal_directions
+    [:east, :west, :north, :south].each do |dir|
+      @snake.turn(dir)
+      assert_equal dir, @snake.direction
+    end
+  end
+
+  def test_turn_invalid_direction
+    assert_raise RuntimeError do
+      @snake.turn(:up) # maybe only invalid for now? :-P
+    end
+  end
+    
+  def test_move_east
+    @snake.turn(:east)
+    @snake.move
+    assert_equal [1, 0], @snake.head
+    assert_equal @snake.head, @snake.tail
+  end
+
+  def test_move_west
+    @snake.turn(:west)
+    @snake.move
+    assert_equal [-1, 0], @snake.head
+    assert_equal @snake.head, @snake.tail
+  end
+
+  def test_move_north
+    @snake.turn(:north)
+    @snake.move
+    assert_equal [0, 1], @snake.head
+    assert_equal @snake.head, @snake.tail
+  end
+
+  def test_move_south
+    @snake.turn(:south)
+    @snake.move
+    assert_equal [0, -1], @snake.head
+    assert_equal @snake.head, @snake.tail
+  end
+
+end
+
+class TestSnakeEdgeCases < Test::Unit::TestCase
+  def test_create_with_one_point
+    snake = Snake.new([3, 4])
+    assert_equal [[3, 4], [3, 4]], snake.segments[0]
+  end
+end
+
+class TestSnakeDefaultDirections < Test::Unit::TestCase
+  def test_nothing
+  end
+end
+
+class TestSingleSegmentSnake < Test::Unit::TestCase
+  def setup
+    @snake = Snake.new([0, 4], [0, 0])
+  end
+
+  def test_length
+    assert_equal 5, @snake.length
+  end
+
+  def test_head
+    assert_equal [0, 4], @snake.head
+  end
+
+  def test_tail
+    assert_equal [0, 0], @snake.tail
+  end
+
+  def test_segment_zero
+    assert_equal [[0, 4], [0, 0]], @snake.segments[0]
+  end
+
+  def test_segments_length
+    assert_equal 1, @snake.segments.length
+  end
+
+  def test_move_north
+    @snake.turn(:north)
+    @snake.move
+    assert_equal [0, 5], @snake.head
+    assert_equal [0, 1], @snake.tail
+  end
+
+  def test_turn_south_is_ignored
+    @snake.turn(:north)
+    @snake.turn(:south)
+    assert_equal :north, @snake.direction
+  end
+
+end
+
+class TestNorthLyingSnakeMovesEast < Test::Unit::TestCase
+  def setup
+    @snake = Snake.new([0, 4], [0, 0])
+    @snake.turn(:east)
+    @snake.move()
+  end
+
+  def test_has_two_segments
+    assert_equal 2, @snake.segments.length
+  end
+
+  def test_length_stays_the_same
+    assert_equal 5, @snake.length
+  end
+end
+
+class TestMultiSegmentSnake < Test::Unit::TestCase
+  def setup
+    @snake = Snake.new([0, 0], [0, 4], [3, 4], [3, 2])
+  end
+
+  def test_length
+    assert_equal 10, @snake.length
+  end
+
+  def test_head
+    assert_equal [0, 0], @snake.head
+  end
+
+  def test_tail
+    assert_equal [3, 2], @snake.tail
+  end
+
+  def test_middle_segment
+    assert_equal [[0, 4], [3, 4]], @snake.segments[1]
+  end
+
+  def test_segments_length
+    assert_equal 3, @snake.segments.length
   end
 end
